46a359ea4c7ddce2e795ec94f28ad6159f965d74
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-04-02 09:27:13 +0200

    Corrige 'FindGlib' pour Win32 si on utilise 'pkg-config'.
    Sous Windows, avec 'vcpkg', 'pkg-config' ne retourne pas tous les chemins
    nécessaires pour les .h. Il faut donc les chercher à la main.

--- build-system/Modules/FindGlib.cmake
+++ build-system/Modules/FindGlib.cmake
@@ -19,18 +19,40 @@ message(STATUS "PKG_GLIB_LIBRARY_DIRS       = ${PKG_GLIB_LIBRARY_DIRS}")
 find_library(GLIB_GLIB_LIBRARIES NAMES glib-2.0 HINTS ${PKG_GLIB_LIBDIR} ${PKG_GLIB_LIBRARY_DIRS})
 find_library(GLIB_GTHREAD_LIBRARIES NAMES gthread-2.0 HINTS ${PKG_GLIB_LIBDIR} ${PKG_GLIB_LIBRARY_DIRS})
 find_library(GLIB_GMODULE_LIBRARIES NAMES gmodule-2.0 HINTS ${PKG_GLIB_LIBDIR} ${PKG_GLIB_LIBRARY_DIRS})
-if(PKG_GLIB_INCLUDE_DIRS)
-  set(Glib_INCLUDE_DIRS ${PKG_GLIB_INCLUDE_DIRS})
-else()
-  find_path(Glib_INCLUDE_DIRS NAMES glib.h PATH_SUFFIXES include HINTS ${PKG_GLIB_INCLUDE_DIRS})
-endif()
+
+# On a besoin de 'glib.h' et 'glibconfig.h' qui peuvent être dans des répertoires différents
+
+#get_filename_component(_GLIB_LIBRARY_DIR ${GLIB_GLIB_LIBRARIES} PATH)
+find_path(GLIBCONFIG_INCLUDE_DIR
+    NAMES glibconfig.h
+    HINTS ${PKG_GLIB_LIBDIR} ${PKG_GLIB_LIBRARY_DIRS} ${PKG_GLIB_INCLUDE_DIRS}
+    PATH_SUFFIXES glib-2.0/include
+)
+find_path(GLIB_INCLUDE_DIR
+    NAMES glib.h
+    HINTS ${PKG_GLIB_INCLUDEDIR}
+          ${PKG_GLIB_INCLUDE_DIRS}
+    PATH_SUFFIXES glib-2.0
+)
+set(Glib_INCLUDE_DIRS ${GLIB_INCLUDE_DIR} ${GLIBCONFIG_INCLUDE_DIR})
+
+#if(PKG_GLIB_INCLUDE_DIRS)
+#  set(Glib_INCLUDE_DIRS ${PKG_GLIB_INCLUDE_DIRS})
+#else()
+#  find_path(Glib_INCLUDE_DIRS NAMES glib.h PATH_SUFFIXES include HINTS ${PKG_GLIB_INCLUDE_DIRS})
+#endif()
+#set(GLIB_FOUND ${PKG_GLIB_FOUND})
 set(Glib_LIBRARIES ${GLIB_GLIB_LIBRARIES} ${GLIB_GTHREAD_LIBRARIES} ${GLIB_GMODULE_LIBRARIES})
-set(GLIB_FOUND ${PKG_GLIB_FOUND})
+
 message(STATUS "Glib_LIBRARIES          = ${Glib_LIBRARIES}")
 message(STATUS "Glib_INCLUDE_DIRS       = ${Glib_INCLUDE_DIRS}")
 
-# Create target interface
-arccon_register_package_library(Glib Glib)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Glib REQUIRED_VARS Glib_INCLUDE_DIRS Glib_LIBRARIES)
+if (Glib_FOUND)
+  # Create target interface
+  arccon_register_package_library(Glib Glib)
+endif()
 
 # ----------------------------------------------------------------------------
 # Local Variables:
